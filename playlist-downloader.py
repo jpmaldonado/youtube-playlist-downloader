9294d2204ca0617a0e21c9a1ccb441c45775b760
Pablo Maldonado <info@pablomaldonado.org>
2018-05-01 21:51:44 +0200

    fixed urllib2 dependency issue

--- playlist-downloader.py
+++ playlist-downloader.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python
 import os, sys, time
 import argparse
-from urllib2 import urlopen
+from urllib.request import urlopen
 
 import pytube  # pip install pytube
 
 
-CS294_playlist_url = "https://www.youtube.com/playlist?list=PLkFD6_40KJIwTmSbCv9OVJB3YaO4sFwkX"
+CS294_playlist_url = "https://www.youtube.com/playlist?list=PLkFD6_40KJIznC9CDbVTjAF2oyt8_VAe3"
 
 
 def get_playlist_links(playlist_url):
-    page_elements = urlopen(playlist_url).readlines()
-    video_elements = [el for el in page_elements if 'pl-video-title-link' in el]  # Filter out unnecessary lines
-    video_urls = [v.split('href="',1)[1].split('" ',1)[0] for v in video_elements]  # Grab the video urls from the elements
-    return ['http://www.youtube.com' + v for v in video_urls]
+	page_elements = [x.decode('utf8') for x in urlopen(playlist_url).readlines()]
+	video_elements = [el for el in page_elements if 'pl-video-title-link' in el]  # Filter out unnecessary lines
+	video_urls = [v.split('href="',1)[1].split('" ',1)[0] for v in video_elements]  # Grab the video urls from the elements
+	return ['http://www.youtube.com' + v for v in video_urls]
 
 
 start_time = time.time()
@@ -39,18 +39,19 @@ else:
     sys.exit(1)
 
 video_urls = get_playlist_links(args.playlisturl)
-confirmation = raw_input('You are about to download {} videos to {}\nWould you like to continue? [Y/n] '.format(
+
+confirmation = input('You are about to download {} videos to {}\nWould you like to continue? [Y/n] '.format(
     len(video_urls), os.path.abspath(args.destination)))
 
 if confirmation.lower() in ['y', '']:
-    for u in video_urls:
-        yt = pytube.YouTube(u)
-        vid = yt.streams.filter(file_extension='mp4').order_by('res').last() # grab the highest resolution mp4 file
+	for u in video_urls:
+		yt = pytube.YouTube(u)
+		vid = yt.streams.filter(mime_type='video/mp4').order_by('resolution').last() # grab the highest resolution mp4 file
 
-        if vid.default_filename in directory_contents:
-            print('Skipping {}'.format(vid.default_filename))
-            continue
-        else:
-            print('Downloading {}'.format(vid.default_filename))
-            vid.download(args.destination)
-            print('Done')
+		if vid.default_filename in directory_contents:
+			print('Skipping {}'.format(vid.default_filename))
+			continue
+		else:
+			print('Downloading {}'.format(vid.default_filename))
+			vid.download(args.destination)
+			print('Done')
